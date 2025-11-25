9d2c3b247a48cc2e8101e3e95a99242b84915e44
kraktus <kraktus@users.noreply.github.com>
2025-11-25 15:29:15 +0100

    commit empty files to start

---
+++ test_binary_fen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import asyncio
+import copy
+import logging
+import os
+import os.path
+import platform
+import sys
+import tempfile
+import textwrap
+import unittest
+import io
+
+import chess
+
+class BinaryFenTest(unittest.TestCase):
+    pass
