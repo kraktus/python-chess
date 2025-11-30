23632cd78f2b0bcd4b7e018710f207d42131d4ef
kraktus <kraktus@users.noreply.github.com>
2025-11-30 15:54:08 +0100

    more encoding tests

--- test_binary_fen.py
+++ test_binary_fen.py
@@ -16,6 +16,7 @@ import chess
 import chess.variant
 import chess.binary_fen
 
+from chess import Board
 from chess.binary_fen import BinaryFen
 
 KOTH = chess.variant.KingOfTheHillBoard
@@ -72,6 +73,72 @@ class BinaryFenTestCase(unittest.TestCase):
             read_value = chess.binary_fen._read_leb128(iter(data))
             self.assertEqual(value, read_value)
 
+# test("handpicked fens roundtrip"):
+#     assertRoundtrip(
+#       Horde,
+#       FullFen("rn1qkb1r/3bn1p1/2p3P1/pPP2P2/P1PPP1P1/P1PP1PPP/PPPPPPPP/PPPPPPPP w kq a6 0 12")
+#     )
+
+#     assertRoundtrip(Antichess, FullFen("8/2p1p2p/2Q1N2B/8/p7/N7/PPP1P1PP/R4B1R b - - 0 13"))
+#     assertRoundtrip(Antichess, FullFen("8/p6p/4p3/1P4P1/Pp4p1/3P4/7P/8 b - a3 0 1"))
+#     assertRoundtrip(Antichess, FullFen("8/p6p/4p3/1P4P1/1p4pP/3P4/P7/8 b - h3 0 1"))
+#     assertRoundtrip(Antichess, FullFen("8/7p/4p3/pP4P1/1p1P2p1/8/P6P/8 w - a6 0 2"))
+#     assertRoundtrip(Antichess, FullFen("8/p7/4p3/1P4Pp/1p1P2p1/8/P6P/8 w - h6 0 2"))
+
+#     assertRoundtrip(Atomic, FullFen("rnbq3r/ppp1p1pp/5p2/3p4/8/8/PPPPPPPP/RNBQKB1R b KQ - 0 4"))
+#     assertRoundtrip(Atomic, FullFen("8/6pp/2p2p1n/3p4/4P3/B6P/3P1PP1/1r2K2R b K - 0 17"))
+
+#     assertRoundtrip(RacingKings, FullFen("8/8/8/8/8/8/krbnNBRK/qrbnNBRQ w - - 0 1"))
+#     assertRoundtrip(RacingKings, FullFen("8/8/8/8/8/6K1/krbnNBR1/qrbnNBRQ b - - 1 1"))
+
+#     assertRoundtrip(KingOfTheHill, FullFen("rnbq1bnr/ppp2ppp/3k4/4p2Q/3PK3/8/PPP2PPP/RNB2BNR b - - 0 7"))
+
+#     assertRoundtrip(
+#       ThreeCheck,
+#       FullFen("1r3rk1/pbp1N1pp/3p1q2/1p2bp2/7P/2PBB1P1/PP3Q1R/R5K1 b - - 3 21 +2+1")
+#     )
+
+#     assertRoundtrip(
+#       Crazyhouse,
+#       FullFen("1r3Q1n/p1kp3p/1p2ppq1/2p2b2/8/3P2P1/PPP1PPBP/R4RK1/NRpnnbb w - - 2 28")
+#     )
+#     assertRoundtrip(Crazyhouse, FullFen("b2nkbnQ~/p1pppp1p/pP1q2p1/r7/8/R5PR/P1PP1P1P/1NBQ1BNK/R w - - 1 2"))
+#     assertRoundtrip(Crazyhouse, FullFen("8/8/8/8/8/8/8/8/ w - - 0 1"))
+#     assertRoundtrip(
+#       Crazyhouse,
+#       FullFen("r~n~b~q~kb~n~r~/pppppppp/8/8/8/8/PPPPPPPP/RN~BQ~KB~NR/ w KQkq - 0 1")
+#     )
+    def test_binary_fen_roundtrip(self):
+        cases = [
+            Board(fen="8/8/8/8/8/8/8/8 w - - 0 1"),
+            Board(fen="8/8/8/8/8/8/8/8 b - - 0 1"),
+            Board(fen="8/8/8/8/8/8/8/8 w - - 0 2"),
+            Board(fen="8/8/8/8/8/8/8/8 b - - 0 2"),
+            Board(fen="8/8/8/8/8/8/8/8 b - - 100 432"),
+            Board(fen="rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1"),
+            Board(fen="4nrk1/1pp3pp/p4p2/4P3/2BB1n2/8/PP3P1P/2K3R1 b - - 1 25"),
+            Board(fen="4nrk1/1pp3pp/p4p2/4P3/2BB1n2/8/PP3P1P/2K3R1 b - - 1 25"),
+            Board(fen="5k2/6p1/8/1Pp5/6P1/8/8/3K4 w - c6 0 1"),
+            Board(fen="4k3/8/8/8/3pP3/8/6N1/7K b - e3 0 1"),
+            Board(fen="r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"),
+            Board(fen="r1k1r2q/p1ppp1pp/8/8/8/8/P1PPP1PP/R1K1R2Q w KQkq - 0 1",chess960=True),
+            Board(fen="r1k2r1q/p1ppp1pp/8/8/8/8/P1PPP1PP/R1K2R1Q w KQkq - 0 1", chess960=True),
+            Board(fen="8/8/8/4B2b/6nN/8/5P2/2R1K2k w Q - 1 1",  chess960=True),
+            Board(fen="2r5/8/8/8/8/8/6PP/k2KR3 w K - 0 2",  chess960=True),
+            Board(fen="4r3/3k4/8/8/8/8/6PP/qR1K1R2 w KQ - 2 1", chess960=True),
+            Board(fen="4rrk1/pbbp2p1/1ppnp3/3n1pqp/3N1PQP/1PPNP3/PBBP2P1/4RRK1 w Ff - 0 3",  chess960=True),
+            Board(fen="8/8/8/1k6/3Pp3/8/8/4KQ2 b - d3 3 1"),
+            Board(fen="r2r3k/p7/3p4/8/8/P6P/8/R3K2R b KQq - 0 4",chess960=True),
+            Board(fen="rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR w Gkq - 4 11", chess960=True),
+            Board(fen="8/8/8/8/8/8/2Rk4/1K6 w - - 0 1"),
+        ]
+        for case in cases:
+            case_fen = case.fen()
+            with self.subTest(fen=case_fen):
+                encoded = BinaryFen.encode(case)
+                decoded = BinaryFen.decode(encoded)
+                self.assertEqual(case_fen, decoded.fen())
+
 
     def test_read_binary_fen_std(self):
         test_cases = [
@@ -84,6 +151,7 @@ class BinaryFenTestCase(unittest.TestCase):
             # TODO FIXME, this is encoded with `standard` variant but with chess960 castling
             # should this be accepted? for now basing on scalachess behavior
             ("8901080000810091ad0d10e1f70007", "r2r3k/p7/3p4/8/8/P6P/8/R3K2R b KQq - 0 4"),
+            ("95dd00000000dd95ad8d000000111111be9e", "r1k1r2q/p1ppp1pp/8/8/8/8/P1PPP1PP/R1K1R2Q w KQkq - 0 1"),
             ("00000002180000308a1c0f030103", "8/8/8/1k6/3Pp3/8/8/4KQ2 b - d3 3 1")
         ]
         for binary_fen, expected_fen in test_cases:
