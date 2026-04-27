cd8e0dac7de70802f63b794845c2b68422ae2b3c
kraktus <kraktus@users.noreply.github.com>
2026-04-27 19:24:11 +0200

    add helper scripts

---
+++ bin/bench.sh
@@ -0,0 +1,4 @@
+
+
+asv run --show-stderr --bench SquareSetSuite --machine Kraktus && \
+asv publish && asv preview && open localhost:8080
---
+++ bin/run_test.sh
@@ -0,0 +1,3 @@
+
+# assume we're in venv
+cd rust_chess && maturin develop && cd - && python3 test.py
