3a8866da0bb9f89938387b0208c92e1bedc4af92
kraktus <kraktus@users.noreply.github.com>
2026-08-02 02:14:13 +0200

    re-enable BoardSuite

--- bin/bench.sh
+++ bin/bench.sh
@@ -1,5 +1,5 @@
 
 
 # TODO, remove the --quick to get thorough bench
-asv run --quick --show-stderr --machine Kraktus --bench PieceSuite --bench MoveSuite --bench SquareSetSuite --bench BaseBoardSuite && \
+asv run --quick --show-stderr --machine Kraktus --bench PieceSuite --bench MoveSuite --bench SquareSetSuite --bench BaseBoardSuite --bench BoardSuite && \
 asv publish && asv preview && open localhost:8080
